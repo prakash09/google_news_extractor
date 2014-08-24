41955f197005a229f522e65f24ae7944700ba95f
prakash <prakash.gbpec@gmail.com>
2014-08-25 00:56:52 +0530

    news.py file added

---
+++ news.py
@@ -0,0 +1,48 @@
+import urllib2
+import smtplib
+import json
+import HTMLParser
+def search():
+	query='Delhi'
+	final_url = ('https://ajax.googleapis.com/ajax/services/search/news?v=1.0&q='+query+'&userip=INSERT-USER-IP')
+	json_obj=urllib2.urlopen(final_url)
+	data=json.load(json_obj)
+	for news1 in data['responseData']['results']:
+		news1['titleNoFormatting']= HTMLParser.HTMLParser().unescape(news1['titleNoFormatting'])
+		if  'relatedStories' in locals():
+			for news2 in news1['relatedStories']:
+				news2['titleNoFormatting']=HTMLParser.HTMLParser().unescape(news2['titleNoFormatting'])
+	send_email(data)
+def send_email(data):
+	FROM = "sender@example.com"
+	TO = ["prakash.kumar94@live.com"] 
+	SUBJECT = "Delhi Headlines"
+	TEXT=""
+	for news3 in data['responseData']['results']:
+		TEXT=TEXT+"\n\n\n "
+		atitle="Headline:"+str(news3['titleNoFormatting'])
+		alink= "link:"+str(news3['unescapedUrl'])
+		apublisher="published by-"+str(news3['publisher'])
+        	TEXT=TEXT+atitle+"\n"+alink+"\n"+apublisher+"\n"
+        	if  'relatedStories' in locals():
+       			for news4 in news3['relatedStories']:
+				a1title= "Headline:"+str(news4['titleNoFormatting'])
+				a1link="link:"+str(news2['unescapedUrl'])
+				a1publisher="published by-"+str(news4['publisher'])+"<p>"
+				TEXT=TEXT+a1title+"\n"+a1link+"\n"+a1publisher+"\n"
+		
+	message = """\
+	From: %s
+	To: %s
+	Subject: %s
+	%s
+	""" % (FROM, ", ".join(TO), SUBJECT, TEXT)
+	# Send the mail
+	server = smtplib.SMTP(host='smtp.gmail.com', port=587)
+	server.starttls()
+	server.login('gmail id', 'password')
+	server.sendmail(FROM,TO,message)
+	server.close()
+				   
+if __name__=="__main__":
+   search()
